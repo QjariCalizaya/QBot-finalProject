06a1a6aa55194ab1107237dc3517837486703f49
qjari calizaya <qjarishekka@outlook.com>
2025-12-25 02:08:51 +0300

    feat(main): main basic bot

--- main.py
+++ main.py
@@ -16,8 +16,13 @@ bot = telebot.TeleBot(TOKEN)
 
 def setup_bot_commands():
     commands = [
-        
+        telebot.types.BotCommand(command='help', description='Здравствуйте. Вы обратились в службу поддержки клиентов. Пожалуйста, введите /start, чтобы начать работу.')
 
 
     ]
     bot.set_my_commands(commands)
+
+
+if __name__ == "__main__":
+    setup_bot_commands()
+    bot.infinity_polling(skip_pending=True)
